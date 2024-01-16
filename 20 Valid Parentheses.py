7d0c4a985c2cf9db3fab61d5f734f7098ded9437
Arthur <saitamafhneh202@gmail.com>
2024-01-16 12:31:49 +0300

    Solve 20

---
+++ 20 Valid Parentheses.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str ) -> bool:
+      
+
+        stack = []
+
+        count = 0
+        
+        for i in s:
+           stack.append(i)
+       
+            
+            
+        
+        
+        
+
+
+
+
+a = Solution()
+
+a.isValid("(){}[]")
+
