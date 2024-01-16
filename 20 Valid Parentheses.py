4787f95190f220380052e70e17cc4046066131c7
Arthur <saitamafhneh202@gmail.com>
2024-01-16 14:01:01 +0300

    None

--- 20 Valid Parentheses.py
+++ 20 Valid Parentheses.py
@@ -1,26 +1,42 @@
-from collections import deque
-
 class Solution:
     def isValid(self, s: str ) -> bool:
       
+        if s == '':
+            return False
+        if len(s) == 1:
+            return False
+
+
+        
 
         stack = []
-
-        count = 0
+        s = list(s)
         
-        for i in s:
-           stack.append(i)
-       
+
+        for i in range(len(s)):
             
-            
+            if s[i] == '(' or s[i] == '[' or s[i] == '{'  :
+                stack.append(s[i])
+            else:
+                if len(stack) == 0:
+                    return False
+                tmp = stack[-1]
+                if tmp == '(':
+                    if s[i] == ')':
+                        stack.pop()
+                    else:
+                        return False
+                elif tmp == '[':
+                    if s[i] == ']':
+                        stack.pop()
+                    else:
+                        return False
+                elif tmp == '{':
+                    if s[i] == '}':
+                        stack.pop()
+                    else:
+                        return False
+
+        # print(True if len(stack) == 0 else False)
+        return True if len(stack) == 0 else False
         
-        
-        
-
-
-
-
-a = Solution()
-
-a.isValid("(){}[]")
-
