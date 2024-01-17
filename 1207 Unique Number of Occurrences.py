bfe8aae23ab65c794d38cea3c8ab1f4a82801e70
Arthur <saitamafhneh202@gmail.com>
2024-01-17 14:50:21 +0300

    None

---
+++ 1207 Unique Number of Occurrences.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def uniqueOccurrences(arr: list[int]) -> bool:
+    count = Counter(arr)
+
+    print(len)
+
+    return len(set(count.values())) == len(count)
+
+
+
+
+
+
+
+uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0])
